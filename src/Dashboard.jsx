7b01f41493a71b364d2f1b2eef147ce602c8fef3
Chris Knapp <chrisknappcc@gmail.com>
2026-09-10 15:57:07 -0500

    Add files via upload

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -279,6 +279,8 @@ function THead({ cols }) {
 const TEAM_MEMBERS = [
   { name: 'Chris Knapp',  ownerId: '78304576',  group: 'bdr',      email: 'cknapp@carecontinuity.com', emails: ['cknapp@carecontinuity.com', 'chrisknappcc@gmail.com'] },
   { name: 'Chiara Pate',  ownerId: '87806380',  group: 'bdr',      email: 'cpate@carecontinuity.com'  },
+  { name: 'Abby Evans',   ownerId: '98527334',  group: 'bdr',      email: 'aevans@carecontinuity.com', hubspotName: 'Abigail Evans' },
+  { name: 'Harley Reed',  ownerId: '98527301',  group: 'bdr',      email: 'hreed@carecontinuity.com'  },
   { name: 'Matt Valin',   ownerId: '76104455',  group: 'vp'       },
   { name: 'Joe Haine',    ownerId: '55217954',  group: 'vp',  hubspotName: 'Joseph Haine' },
   { name: 'Tim Grisham',  ownerId: '83862037',  group: 'vp'       },
